08ee964a3f1b83b2b2940d241ce0727d5e50e79e
Your Name <you@example.com>
2023-10-31 13:42:07 -0700

    definir una nueva estructura llamada struct dog con tres elementos (name, age, owner)

---
+++ structures_typedef/dog.h
@@ -0,0 +1,33 @@
+nclude <stdio.h>
+#include "dog.h"
+
+/**
+*init_dog - Function that initialize a variable of type `struct dog`
+*@d: type struct dog
+*@name: name for the dog of type struct dog
+*@age: age for the dog of type struct dog
+*@owner: owner of the dog of type struct dog
+*Return: void
+*/
+
+#ifndef DOG_H
+#define DOG_H
+
+struct dog {
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
+
+int main(void)
+{
+	struct dog my_dog;
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+	return (0);
+}
