48aaddea11d71879bda87ec95b62d9624e67213b
Your Name <you@example.com>
2023-10-30 18:54:43 -0700

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - print using puts function
  * Return: 0
@@ -7,6 +6,6 @@
 
 int main(void)
 {
-	    puts("\"Programming is like building a multilingual puzzle");
-	    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
